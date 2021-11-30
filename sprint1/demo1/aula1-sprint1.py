cfdb4fa153af15d1330ef26d1dee3f574fe4f560
victorvrmatos <victor.matos@kenzie.com.br>
2021-11-30 11:27:52 -0300

    demo-1 30/nov/2021

---
+++ sprint1/demo1/aula1-sprint1.py
@@ -0,0 +1,62 @@
+# #!/usr/bin/python3
+# 
+# # Variáveis são chamadas de name: texto
+# texto = "kenzie"
+# 
+# num1 = 40
+# num2 = 60
+# 
+# print(num1 == num2)
+# 
+# # erro de configuração de variável: /bin/sh: 1: python: not found
+# print("TEXTO NOVO")
+# 
+# # TIPOS EM PYTHON
+# 
+# # Inteiro: 1, 5, //
+# int
+# 
+# # Float, 1.0, 5.0 /
+# float
+# 
+# # Tupla
+# tuple
+# 
+# # Lista: array,
+# list
+# 
+# # Texto: string
+# str
+# 
+# 
+# # CONVERSÃO DE NÚMEROS PARA SINAIS OPOSTOS: TILDE
+# print(~5)
+# print(~6)
+# print(6 * -1)
+
+'texto'
+"texto"
+""" texto """
+''' texto '''
+
+
+# INDICES DE STRINGS
+"KENZIE ACADEMY"
+"012345678910111213"
+
+
+idade = input("Digite sua idade: ")
+print(idade)
+print(type(idade))
+
+# PEDE ENTRADA PARA USUÁRIO
+# NOME: input()
+# SOBRENOME:
+# IDADE:
+# CIDADE:
+
+# RETORNO
+# Olá [nome].
+# Bem vindo à Kenzie.
+# Você tem x idade, ano que vem você terá y idade.
+# nome. você mora em uma cidade bonita, [nome da cidade]. e em 5 anos você terá z idade
